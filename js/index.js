f260140ef76bf5d6eae115fe0e73ff2643e64c0a
tadeoac <tadeoac.3@hotmail.com>
2020-02-29 02:38:42 -0600

    second animations

--- js/index.js
+++ js/index.js
@@ -16,57 +16,72 @@ var swiper = new Swiper('.swiper-container', {
         slideChangeTransitionStart:function(){
             console.log(this.realIndex);
             if(this.realIndex == 0) {
-                $secondTitle.classList.remove("second-slide-animation");
+                $secondTitle.classList.remove("title-slide-animation");
+                $principalTitle.classList.add('principal-title-animation');
             }
             if(this.realIndex == 1) {
-                $secondTitle.classList.add("second-slide-animation");
-                $thirdTitle.classList.remove("third-slide-animation");
+                $secondTitle.classList.add("title-slide-animation");
+                $thirdTitle.classList.remove("title-slide-animation");
+                $principalTitle.classList.remove('principal-title-animation');
             }
             if(this.realIndex == 2) {
-                $thirdTitle.classList.add("third-slide-animation");
-                $secondTitle.classList.remove("second-slide-animation");
-                $fourthTitle.classList.remove("fourth-slide-animation");
+                $thirdTitle.classList.add("title-slide-animation");
+                $secondTitle.classList.remove("title-slide-animation");
+                $fourthTitle.classList.remove("title-slide-animation");
                 $fanSpin.classList.remove("fan-img-animation");
+                $hatAppear.classList.remove("hat-appear-animation");
             }
             if(this.realIndex == 3) {
-                $fourthTitle.classList.add("fourth-slide-animation");
-                $thirdTitle.classList.remove("third-slide-animation");
-                $fifthTitle.classList.remove("fifth-slide-animation");
+                $fourthTitle.classList.add("title-slide-animation");
+                $thirdTitle.classList.remove("title-slide-animation");
+                $fifthTitle.classList.remove("title-slide-animation");
                 $fanSpin.classList.add("fan-img-animation");
+                $hatAppear.classList.add("hat-appear-animation");
             }
             if(this.realIndex == 4) {
                 animateYears("servYears", 0, 8, 4000);
-                $fifthTitle.classList.add("fifth-slide-animation");
-                $fourthTitle.classList.remove("fourth-slide-animation");
-                $sixthTitle.classList.remove("sixth-slide-animation");
+                $fifthTitle.classList.add("title-slide-animation");
+                $fourthTitle.classList.remove("title-slide-animation");
+                $sixthTitle.classList.remove("title-slide-animation");
                 $fanSpin.classList.remove("fan-img-animation");
+                $hatAppear.classList.remove("hat-appear-animation");
             }
             if(this.realIndex == 5) {
-                $sixthTitle.classList.add("sixth-slide-animation");
-                $fifthTitle.classList.remove("fifth-slide-animation");
-                $seventhTitle.classList.remove("seventh-slide-animation");
+                $sixthTitle.classList.add("title-slide-animation");
+                $fifthTitle.classList.remove("title-slide-animation");
+                $seventhTitle.classList.remove("title-slide-animation");
+                $leftMachine.classList.remove("left-machine-animation");
+                $centerMachine.classList.remove("center-machine-animation");
+                $rightMachine.classList.remove("right-machine-animation");
             }
             if(this.realIndex == 6) {
-                $seventhTitle.classList.add("seventh-slide-animation");
-                $sixthTitle.classList.remove("sixth-slide-animation");
-                $eighthTitle.classList.remove("eighth-slide-animation");
+                $seventhTitle.classList.add("title-slide-animation");
+                $sixthTitle.classList.remove("title-slide-animation");
+                $eighthTitle.classList.remove("title-slide-animation");
+                $leftMachine.classList.add("left-machine-animation");
+                $centerMachine.classList.add("center-machine-animation");
+                $rightMachine.classList.add("right-machine-animation");
             }
             if(this.realIndex == 7) {
-                $eighthTitle.classList.add("eighth-slide-animation");
-                $seventhTitle.classList.remove("seventh-slide-animation");
-                $ninethTitle.classList.remove("nineth-slide-animation");
+                $eighthTitle.classList.add("title-slide-animation");
+                $seventhTitle.classList.remove("title-slide-animation");
+                $ninethTitle.classList.remove("title-slide-animation");
+                $leftMachine.classList.remove("left-machine-animation");
+                $centerMachine.classList.remove("center-machine-animation");
+                $rightMachine.classList.remove("right-machine-animation");
             }
             if(this.realIndex == 8) {
-                $ninethTitle.classList.add("nineth-slide-animation");
-                $eighthTitle.classList.remove("eighth-slide-animation");
+                $ninethTitle.classList.add("title-slide-animation");
+                $eighthTitle.classList.remove("title-slide-animation");
             }
             if(this.realIndex == 9) {
-                $ninethTitle.classList.remove("nineth-slide-animation");
+                $ninethTitle.classList.remove("title-slide-animation");
             }
         }
     }
 });
 
+const $principalTitle = document.getElementById('principal-title');
 const $secondTitle = document.getElementById('second-title');
 const $thirdTitle = document.getElementById('third-title');
 const $fourthTitle = document.getElementById('fourth-title');
@@ -76,6 +91,10 @@ const $seventhTitle = document.getElementById('seventh-title');
 const $eighthTitle = document.getElementById('eighth-title');
 const $ninethTitle = document.getElementById('nineth-title');
 const $fanSpin = document.getElementById('fan-img');
+const $hatAppear = document.getElementById('hat-appear');
+const $leftMachine = document.getElementById('left-machine');
+const $centerMachine = document.getElementById('center-machine');
+const $rightMachine = document.getElementById('right-machine');
 
 var onloadCallback = function() {
     alert("grecaptcha is ready!");
